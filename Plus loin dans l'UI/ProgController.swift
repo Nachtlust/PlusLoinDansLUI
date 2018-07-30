7bc5b86a0ce24a35d55154b682d28ccb6a090609
Couvelard Mathieu <couvelard.mathieu@gmail.com>
2018-07-30 11:26:24 +0200

    UILabel

--- Plus loin dans l'UI/ProgController.swift
+++ Plus loin dans l'UI/ProgController.swift
@@ -11,25 +11,24 @@ import UIKit
 class ProgController: UIViewController {
 
     var monPremierUIView: UIView?
+    var monPremierLabel: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         monPremierUIView = UIView(frame: CGRect(x: 30, y: 60, width: 200, height: 50))
         monPremierUIView?.backgroundColor = UIColor(red: 30 / 255, green: 123 / 255, blue: 90 / 255, alpha: 1)
         monPremierUIView?.layer.cornerRadius = 25
-        guard monPremierUIView != nil else { return }
+        // guard monPremierUIView != nil else { return }
         view.addSubview(monPremierUIView!)
         print(monPremierUIView?.frame)
         print(monPremierUIView?.bounds)
         
-        let secondeVue = UIView(frame: monPremierUIView!.frame)
-        secondeVue.center.y += 100
-        view.addSubview(secondeVue)
-        secondeVue.backgroundColor = UIColor.red
-        
-        let troisiemeVue = UIView(frame: monPremierUIView!.bounds)
-        secondeVue.center.y += 200
-        view.addSubview(troisiemeVue)
-        troisiemeVue.backgroundColor = UIColor.brown
+        monPremierLabel = UILabel(frame: CGRect(x: 0, y: monPremierUIView!.frame.maxY + 20, width: view.frame.width, height: 50))
+        monPremierLabel?.text = "Coucou je suis un texte"
+        monPremierLabel?.numberOfLines = 1
+        monPremierLabel?.textColor = UIColor.white
+        monPremierLabel?.font = UIFont(name: "Chalkduster", size: 20)
+        monPremierLabel?.textAlignment = .center
+        view.addSubview(monPremierLabel!)
     }
 }
